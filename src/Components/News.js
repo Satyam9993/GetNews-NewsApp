5c8508769c5e450cc10e6e8c8f4978cc77007af8
Satyam9993 <82694936+Satyam9993@users.noreply.github.com>
2021-12-01 00:03:35 +0530

    yes

--- src/Components/News.js
+++ src/Components/News.js
@@ -15,25 +15,31 @@ export class News extends Component {
 
     async componentDidMount(){
         // console.log("cdn");
-        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=1";
+        let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=1&pagesize=15";
         let data = await fetch(url);
         let parsedata = await data.json();
         // console.log(parsedata);
-        this.setState({article: parsedata.articles});
+        this.setState({article: parsedata.articles, totalResults: parsedata.totalResults});
     }
     handlenextClick= async() => {
         // console.log("cdn");
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=${this.state.page+1}`;
-        let data = await fetch(url);
-        let parsedata = await data.json();
-        this.setState({article: parsedata.articles});
-        this.setState({
-            page: this.state.page+1
-        })
+        if(this.state.page+1 > Math.ceil(this.state.totalResults/15))
+        {
+            
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=${this.state.page+1}&pagesize=15`;
+            let data = await fetch(url);
+            let parsedata = await data.json();
+            this.setState({article: parsedata.articles});
+            this.setState({
+                page: this.state.page+1
+            })
+        }
     }
 
     handlepreviousClick= async ()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=${this.state.page-1}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=84234aa687874695bcc6e120995a740a&page=${this.state.page-1}&pagesize=15`;
         let data = await fetch(url);
         let parsedata = await data.json();
         this.setState({article: parsedata.articles});
@@ -50,13 +56,13 @@ export class News extends Component {
                 <div className="row my-3">
                 {this.state.article.map((element)=>{
                     return <div className="col-md-4 my-2" key={element.url}>
-                    <NewsItem title={element.title} description={element.description == null ?"": element.description.slice(0, 100)+".."} imageurl={element.urlToImage}  newsUrl={element.url}/>
+                    <NewsItem title={element.title} description={element.description == null ?"": element.description.slice(0, 100)+".."} imageurl={element.urlToImage==null ? "https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60": element.urlToImage}  newsUrl={element.url}/>
                     </div>
                 })}
                 </div>
                 <div className="container d-flex justify-content-center">
                 <button disabled={this.state.page <= 1 ? true : false} type="button" class="btn btn-dark mx-1" onClick={this.handlepreviousClick}>&larr; previous </button>
-                <button type="button" class="btn btn-dark mx-1" onClick={this.handlenextClick}>next &rarr;</button>       
+                <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/15)? true:false} type="button" class="btn btn-dark mx-1" onClick={this.handlenextClick}>next &rarr;</button>       
                 </div> 
                 </div>
             </div>
